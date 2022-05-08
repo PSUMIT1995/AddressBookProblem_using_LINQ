585b9b41e3fae4af57e59cc2a529cc44ee05c1d5
PSUMIT1995 <psumit191.sp@gmail.com>
2022-05-08 16:52:38 +0530

    UC1 : Creating AddressBook

---
+++ AddressBookLINQ/AddressBookLINQ/AddressBookReview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLINQ
+{
+    //create contact details 
+    public class AddressBookReview
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public double Zip { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
