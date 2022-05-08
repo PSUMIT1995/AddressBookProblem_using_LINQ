0c4bd55f270b072adcb6c9a9497001bc2cabb383
PSUMIT1995 <psumit191.sp@gmail.com>
2022-05-08 17:24:57 +0530

    UC3 : Inserting Data to AddressBook

--- AddressBookLINQ/AddressBookLINQ/AddressBookManagement.cs
+++ AddressBookLINQ/AddressBookLINQ/AddressBookManagement.cs
@@ -20,6 +20,14 @@ namespace AddressBookLINQ
             dataTable.Columns.Add("Zip", typeof(double));
             dataTable.Columns.Add("PhoneNumber", typeof(double));
             dataTable.Columns.Add("Email", typeof(string));
+
+            // Creating rows and adding data into row
+            dataTable.Rows.Add("Sumit", "Patel", "Ugwa", "Akola", "Maharashtra", 444006, 9890989065, "Sumit121@gmail.com");
+            dataTable.Rows.Add("Viraj", "Patel", "Akola", "Vidarbha", "Maharshtra", 444008, 9565956263, "Viraj11@gmail.com");
+            dataTable.Rows.Add("Rohit", "Sharma", "Mumbai", "NaviMumbai", "MH", 111004, 8546365412, "Rohit10@gmail.com");
+            dataTable.Rows.Add("Virat", "Kohli", "Mall", "Delhii", "Dehli", 555645, 9874562635, "Virat14@gmail.com");
+            dataTable.Rows.Add("Rahul", "Kl", "PHmall", "Pune", "Maharashtra", 444008, 9896562635, "Rahul005@gmail.com");
+            dataTable.Rows.Add("Sachin", "Singh", "PNP", "Vai", "Goa", 444056, 9856362563, "Sachin10@gmail.com");
         }
     }
 }
